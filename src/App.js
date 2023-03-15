ffebeee51c1f1efa1c12786c023be5fef88b8723
abdurraman-gad <abdelrhaman.g@storexweb.com>
2023-03-15 22:06:17 +0200

    add select unit grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,303 @@
-import logo from './logo.svg';
 import './App.css';
+import Units from './components/Units';
+
+
 
 function App() {
-  return (
+  let units =[
+    {
+      "id": 1,
+      "direction": "LEFT",
+      "level": "UPPER",
+      "order": 1,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 2,
+      "direction": "LEFT",
+      "level": "UPPER",
+      "order": 2,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 3,
+      "direction": "LEFT",
+      "level": "UPPER",
+      "order": 3,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 4,
+      "direction": "LEFT",
+      "level": "UPPER",
+      "order": 4,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 5,
+      "direction": "LEFT",
+      "level": "UPPER",
+      "order": 5,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 6,
+      "direction": "LEFT",
+      "level": "UPPER",
+      "order": 6,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 7,
+      "direction": "LEFT",
+      "level": "UPPER",
+      "order": 7,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 8,
+      "direction": "LEFT",
+      "level": "UPPER",
+      "order": 8,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 9,
+      "direction": "RIGHT",
+      "level": "UPPER",
+      "order": 1,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 10,
+      "direction": "RIGHT",
+      "level": "UPPER",
+      "order": 2,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 11,
+      "direction": "RIGHT",
+      "level": "UPPER",
+      "order": 3,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 12,
+      "direction": "RIGHT",
+      "level": "UPPER",
+      "order": 4,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 13,
+      "direction": "RIGHT",
+      "level": "UPPER",
+      "order": 5,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 14,
+      "direction": "RIGHT",
+      "level": "UPPER",
+      "order": 6,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 15,
+      "direction": "RIGHT",
+      "level": "UPPER",
+      "order": 7,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 16,
+      "direction": "RIGHT",
+      "level": "UPPER",
+      "order": 8,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 17,
+      "direction": "LEFT",
+      "level": "LOWER",
+      "order": 1,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 18,
+      "direction": "LEFT",
+      "level": "LOWER",
+      "order": 2,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 19,
+      "direction": "LEFT",
+      "level": "LOWER",
+      "order": 3,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 20,
+      "direction": "LEFT",
+      "level": "LOWER",
+      "order": 4,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 21,
+      "direction": "LEFT",
+      "level": "LOWER",
+      "order": 5,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 22,
+      "direction": "LEFT",
+      "level": "LOWER",
+      "order": 6,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 23,
+      "direction": "LEFT",
+      "level": "LOWER",
+      "order": 7,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 24,
+      "direction": "LEFT",
+      "level": "LOWER",
+      "order": 8,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 25,
+      "direction": "RIGHT",
+      "level": "LOWER",
+      "order": 1,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 26,
+      "direction": "RIGHT",
+      "level": "LOWER",
+      "order": 2,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 27,
+      "direction": "RIGHT",
+      "level": "LOWER",
+      "order": 3,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 28,
+      "direction": "RIGHT",
+      "level": "LOWER",
+      "order": 4,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 29,
+      "direction": "RIGHT",
+      "level": "LOWER",
+      "order": 5,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 30,
+      "direction": "RIGHT",
+      "level": "LOWER",
+      "order": 6,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 31,
+      "direction": "RIGHT",
+      "level": "LOWER",
+      "order": 7,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  },
+  {
+      "id": 32,
+      "direction": "RIGHT",
+      "level": "LOWER",
+      "order": 8,
+      "deleted_at": null,
+      "created_at": "2023-03-07T22:00:23.000000Z",
+      "updated_at": "2023-03-07T22:00:23.000000Z"
+  }
+];
+
+   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Units   units={units}  />
     </div>
   );
 }
---
+++ src/components/Units.jsx
@@ -0,0 +1,218 @@
+import React from "react";
+
+
+export default function Units(units) {
+
+
+
+   return <div className="border-2">
+
+      <div className="grid-container">
+
+
+         <div className="grid-item-upper-left-8">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+
+         <div className="grid-item-upper-left-7 ">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-upper-left-6">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-upper-left-5">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-upper-left-4">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-upper-left-3">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-upper-left-2">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-upper-left-1">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+
+
+         <div className="grid-item-upper-right-1">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+
+         <div className="grid-item-upper-right-2 ">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-upper-right-3">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-upper-right-4">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-upper-right-5">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-upper-right-6">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-upper-right-7">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-upper-right-8">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+
+         <div className="grid-item-lower-left-8">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+
+         <div className="grid-item-lower-left-7">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-lower-left-6">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-lower-left-5">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-lower-left-4">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-lower-left-3">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-lower-left-2">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-lower-left-1">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+
+         <div className="grid-item-lower-right-1">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+
+         <div className="grid-item-lower-right-2 ">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-lower-right-3">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-lower-right-4">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-lower-right-5">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-lower-right-6">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-lower-right-7">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+         <div className="grid-item-lower-right-8">
+            <i
+               className="fa-solid fa-tooth"
+            >
+            </i>
+         </div>
+
+      </div>
+
+   </div>
+
+}
